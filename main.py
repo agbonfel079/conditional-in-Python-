71af02047f9eb7101ae82b2409b58fa97b56c6db
Felix Agbontaen <felixagbontaen96@gmail.com>
2021-02-10 21:31:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# x =  15
+# y =7
+
+# if x > y:
+#    print('Yes')
+
+  #=============================
+# x = 5
+# y = 7
+# if x <  y:
+#  print('No')
+
+# if 'Hello' in 'Hello world!':
+#   print("Yes")
+  #=============================
+  
+# x = 7
+# y = 7
+
+# if  x  >  y:
+#   print('yes it is greater')
+# elif x < y:
+#  print('no it is less')
+# else:
+#  print('Neither')
+ 
+  #=============================
+
+'''
+Nested if statements
+'''
+
+# num = -3
+# if num >= 0:
+#   if num == 0:
+#     print("zero")
+#   else: 
+#    print("Positive number")
+# else:
+#     print('negative number')
+
+ #=============================
+
+'''
+ one-line if statements
+ '''
+# if 7 > 5: print('yes it is')
+
+# if 7 > 5: print('yes it is') else: print('yes')
+
+#=============================
+'''
+Ternary Operator
+'''
+# print('yes') if 7 > 5 else print('No')
+
+# if 7 > 5:
+#   print('Yes')
+# else:
+#   print('No')
+
+  #=============================
+
+if 100 > 50:
+    print('yes')
